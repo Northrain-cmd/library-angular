48ec18f4381b0690ed24524ad225d4ad93a42e37
Kris <sichinava.ch@gmail.com>
2020-06-12 15:51:23 +0300

    App Built

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDHp-c9_C05EtNRvUnyosb66dwerjUFxrc",
+    authDomain: "library-e52ce.firebaseapp.com",
+    databaseURL: "https://library-e52ce.firebaseio.com",
+    projectId: "library-e52ce",
+    storageBucket: "library-e52ce.appspot.com",
+    messagingSenderId: "481232985305",
+    appId: "1:481232985305:web:7dde1df53b19cc06e972c8"
+  },
 };
